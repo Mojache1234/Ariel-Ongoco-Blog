5ff144b90af3bd99c98532c076926a2c3dba4ca1
Mojache1234 <arielvongoco@gmail.com>
2018-09-25 22:24:21 -0700

    Blog

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('a[href*="#"').on('click', function(e){
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length ? target :  $('[name=' + this.hash.slice(1) + ']');
+    }
+    if (target.length) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top - 55
+        }, 1000, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+                return false;
+            } else {
+                $target.attr('tabindex', '-1');
+                $target.focus();
+            }
+        });
+    }
+});
+
+$('.dropdown-toggle').dropdown();
